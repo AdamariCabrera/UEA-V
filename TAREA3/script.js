6a686ca834dc6ce9a0debadd99da66f0d1bb82a2
AdamariCabrera <162341584+AdamariCabrera@users.noreply.github.com>
2025-03-10 20:04:04 -0600

    TAREA3

---
+++ TAREA3/script.js
@@ -0,0 +1,8 @@
+let p1= "Desde mi perspectiva y el como veo que mis amigos se involucran con los medios digitaes yblas redes, veo que desde pequeños etuvimos en contacto con ella, de pequeños viendo la television y algunos ya teniendo Facebook, despues ya adolescentes ya dentro del mundo de las redes sociales completamente, pir influencias exteriores como hermanos o amigos, pero completamente todos ya sabiamos manejar una red socia, desde crear una cuenta de Instagram, hasta crear un Gmail, al menos asi empezo mi inmersion a la era digital.";
+let p2= "Estoy aprendiendo acerca de como programar, no de una manera como un Ingenierx lo haria, o al menos ese no es mi enfoque, sino de una manera “creativa” y me gusta el echo de aprender algo que no sabia que era buena o que hasta cierto punto le entiendo, me gusta que puedo crear con la programacion. Al crear este tipo de Arte o PIezas, no solo existirian por le momento o por el objetivo, tampoco me gustaria limitarme a solo eso, va mas alla de cear un recuerdo de mis ideas o de mis ideales en el momnto, me ayuda a crear una identidad aparte de lo que creo, si no de lo que puedo crear apartir de la programacion. ";
+
+document.getElementById ('parr1').innerHTML= p1;
+document.getElementById ('parr2').innerHTML= p2;
+
+console.log(p1.length);
+console.log(p2.length);

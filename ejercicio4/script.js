178cf930e302cd1662083d86555e2c262b27835f
AdamariCabrera <2233033971@correo.ler.uam.mx>
2025-03-18 08:56:14 -0600

    ejercicio 4

---
+++ ejercicio4/script.js
@@ -0,0 +1,18 @@
+//variable
+let txt="Este texto es de prueba para ver que funcione el codigo. Este parrafo es de prueba y ya.";
+
+let txt2= txt.toUpperCase();
+
+let txt3= txt2.replace("PARRAFO", "STRINGS");
+
+//documento
+document.getElementById('cadena').innerHTML=txt;
+document.getElementById('cadena2').innerHTML=txt2;
+document.getElementById('cadena3').innerHTML=txt3;
+
+//console
+console.log(txt.at(9));
+console.log(toUpperCase());
+
+
+window.alert("jejeje");
